41e5a880ebfc5c7a30dffc2a0574902b283ce442
Sudarshan shinde <shindesudarshan070@gmail.com>
2020-11-23 11:53:04 +0530

    total price add

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,15 +4,29 @@ export default function Cart({ cartItems }) {
   return (
     <div>
       {cartItems.map((item, i) => {
-        let { sku, title, quantity } = item;
+        let { sku, title, quantity, price } = item;
         return (
           <div key={i}>
             <img src={`/static/products/${sku}_2.jpg`} alt="" />
             <p>{title}</p>
             <p>{quantity}</p>
+            <p>{price}</p>
           </div>
         );
       })}
+      <div>
+        {cartItems.length > 0 ? (
+          <p>
+            Total:
+            {cartItems.reduce((acc, cv) => {
+              acc = acc + cv.price * cv.quantity;
+              return acc;
+            }, 0)}
+          </p>
+        ) : (
+          ""
+        )}
+      </div>
     </div>
   );
 }

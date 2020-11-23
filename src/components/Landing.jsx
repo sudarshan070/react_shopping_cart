551c2ad06d040313c287a5fa8ef16bf1578096d1
Sudarshan shinde <shindesudarshan070@gmail.com>
2020-11-23 09:09:20 +0530

    shows unique sizes

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,26 +2,37 @@ import React from "react";
 import { products } from "../data.json";
 
 export default function Landing() {
+  let sizes = [
+    ...new Set(products.map((product) => product.availableSizes).flat()),
+  ];
+  console.log(sizes);
   return (
-    <div>
-      {products.map((product, i) => {
-        const { sku, title, price, installments } = product;
-        return (
-          <div key={i}>
-            <img src={`static/products/${sku}_1.jpg`} alt="" />
-            <h2>{title}</h2>
-            <p>{price}</p>
-            {installments > 0 ? (
-              <p className="text-secondary">
-                or {installments} X $
-                {parseFloat(price / installments).toFixed(2)}
-              </p>
-            ) : (
-              "No Installments"
-            )}
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <aside>
+        {sizes.map((size) => {
+          return <button>{size}</button>;
+        })}
+      </aside>
+      <div>
+        {products.map((product, i) => {
+          const { sku, title, price, installments } = product;
+          return (
+            <div key={i}>
+              <img src={`static/products/${sku}_1.jpg`} alt="" />
+              <h2>{title}</h2>
+              <p>{price}</p>
+              {installments > 0 ? (
+                <p className="text-secondary">
+                  or {installments} X $
+                  {parseFloat(price / installments).toFixed(2)}
+                </p>
+              ) : (
+                "No Installments"
+              )}
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }

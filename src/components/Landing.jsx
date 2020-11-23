76cd688680f094017914a91b136ab649bf3f6f24
Sudarshan shinde <shindesudarshan070@gmail.com>
2020-11-23 09:03:15 +0530

    product display

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,10 +1,27 @@
 import React from "react";
-
+import { products } from "../data.json";
 
 export default function Landing() {
   return (
     <div>
-      <h1>React shopping cart</h1>
+      {products.map((product, i) => {
+        const { sku, title, price, installments } = product;
+        return (
+          <div key={i}>
+            <img src={`static/products/${sku}_1.jpg`} alt="" />
+            <h2>{title}</h2>
+            <p>{price}</p>
+            {installments > 0 ? (
+              <p className="text-secondary">
+                or {installments} X $
+                {parseFloat(price / installments).toFixed(2)}
+              </p>
+            ) : (
+              "No Installments"
+            )}
+          </div>
+        );
+      })}
     </div>
   );
 }

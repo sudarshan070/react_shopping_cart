269a76618cff338ce9f46bf11fba68cbb3bb94fa
Sudarshan shinde <shindesudarshan070@gmail.com>
2020-11-24 08:56:42 +0530

    free shipping tag add

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,18 +4,33 @@ export default function Main({ products, setCartItems, highToLow, LowToHigh }) {
   return (
     <>
       <div className="d-flex justify-content-between">
-        <p>{`${products.length} product`}</p>
+        <p>{`${products.length} product(s) found`}</p>
         <div>
-          <button onClick={() => highToLow()}>Low to High</button>
-          <button onClick={() => LowToHigh()}> High to Low</button>
+          <button
+            className="btn btn-secondary btn-sm mr-2"
+            onClick={() => highToLow()}
+          >
+            Low to High
+          </button>
+          <button
+            className="btn btn-secondary btn-sm mr-2"
+            onClick={() => LowToHigh()}
+          >
+            High to Low
+          </button>
         </div>
       </div>
 
       <div className="d-flex flex-wrap">
         {products.map((product, i) => {
-          const { sku, title, price, installments } = product;
+          const { sku, title, price, installments, isFreeShipping } = product;
           return (
             <div key={i} className="text-center p-3 product-cart mt-3">
+              <div className="free-shipping">
+                {isFreeShipping === true ? (
+                  <button className=" free-shipping-btn">FreeShipping</button>
+                ) : null}
+              </div>
               <img src={`static/products/${sku}_1.jpg`} alt={title} />
               <h2 className="product-title">{title}</h2>
               <p className="product-price">
